c359c605bfdd12bdf4ad0928fbf7dc2f3f5d25ca
mcc-devel <jettzheng@foxmail.com>
2021-08-28 10:52:45 +0800

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -8,4 +8,4 @@ class fileNotFoundError(RuntimeError):
     def __init__(self, filename):
         self.f = filename
     def __str__(self):
-        return 'File %s not found, please exit manually and fix problem/refresh database!' % self.f
+        return 'File %s not found, please exit manually and fix problem (or maybe just refresh the database)!' % self.f
